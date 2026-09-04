70abd146f19f8f50a5b3333fd8db93ee396fa8ec
Sureka7557 <sureka04102005@gmail.com>
2026-09-04 20:05:52 +0530

    Connect Clerk webhook

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import clerkwebhook from "./controllers/webhooks.js";
 
 dotenv.config();
 
@@ -11,8 +12,16 @@ const app = express();
 await connectDB();
 
 app.use(cors());
+
+
+app.post(
+  "/api/clerk",
+  express.raw({ type: "application/json" }),
+  clerkwebhook
+);
+
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 app.get("/", (_req, res) => {
   res.json({
